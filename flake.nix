27aa933060db0efc5bb843f87c0f3b33aa99a20a
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2025-12-01 15:17:22 +0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Advent of Code 2025";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, ... }: 
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [ 
+            gleam
+            erlang
+          ];
+
+          # shellHook = ''
+          #   source .env
+          # '';
+        };
+      }
+    );
+}
